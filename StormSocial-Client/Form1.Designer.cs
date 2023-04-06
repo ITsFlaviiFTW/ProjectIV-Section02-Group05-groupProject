451562cf82fbae3164b886fc8b72989dc3011ca3
Lance Yorke <lance.c.yorke@gmail.com>
2023-04-06 16:48:12 -0400

    ChatUI design finished maybe I think

--- StormSocial-Client/Form1.Designer.cs
+++ StormSocial-Client/Form1.Designer.cs
@@ -31,13 +31,13 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.Logo = new System.Windows.Forms.PictureBox();
             this.SloganPanel = new System.Windows.Forms.Panel();
-            this.SloganText = new System.Windows.Forms.TextBox();
-            this.UsernameLabel = new System.Windows.Forms.Label();
-            this.PasswordLabel = new System.Windows.Forms.Label();
-            this.UsernamTextBox = new System.Windows.Forms.TextBox();
-            this.PasswordTextBox = new System.Windows.Forms.TextBox();
-            this.LoginButton = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.UsernamTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.SloganText = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
             this.SloganPanel.SuspendLayout();
             this.SuspendLayout();
@@ -66,64 +66,65 @@
             this.SloganPanel.Size = new System.Drawing.Size(605, 328);
             this.SloganPanel.TabIndex = 2;
             // 
-            // SloganText
-            // 
-            this.SloganText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
-            this.SloganText.Location = new System.Drawing.Point(21, 136);
-            this.SloganText.Name = "SloganText";
-            this.SloganText.Size = new System.Drawing.Size(374, 23);
-            this.SloganText.TabIndex = 0;
-            this.SloganText.Text = "Don\'t weather the storm alone - connect with others on Storm Social.";
-            // 
-            // UsernameLabel
-            // 
-            this.UsernameLabel.AutoSize = true;
-            this.UsernameLabel.Location = new System.Drawing.Point(214, 186);
-            this.UsernameLabel.Name = "UsernameLabel";
-            this.UsernameLabel.Size = new System.Drawing.Size(63, 15);
-            this.UsernameLabel.TabIndex = 1;
-            this.UsernameLabel.Text = "Username:";
-            // 
-            // PasswordLabel
-            // 
-            this.PasswordLabel.AutoSize = true;
-            this.PasswordLabel.Location = new System.Drawing.Point(214, 224);
-            this.PasswordLabel.Name = "PasswordLabel";
-            this.PasswordLabel.Size = new System.Drawing.Size(60, 15);
-            this.PasswordLabel.TabIndex = 2;
-            this.PasswordLabel.Text = "Password:";
-            // 
-            // UsernamTextBox
-            // 
-            this.UsernamTextBox.Location = new System.Drawing.Point(283, 183);
-            this.UsernamTextBox.Name = "UsernamTextBox";
-            this.UsernamTextBox.Size = new System.Drawing.Size(100, 23);
-            this.UsernamTextBox.TabIndex = 3;
-            // 
-            // PasswordTextBox
-            // 
-            this.PasswordTextBox.Location = new System.Drawing.Point(283, 221);
-            this.PasswordTextBox.Name = "PasswordTextBox";
-            this.PasswordTextBox.Size = new System.Drawing.Size(100, 23);
-            this.PasswordTextBox.TabIndex = 4;
-            // 
-            // LoginButton
-            // 
-            this.LoginButton.Location = new System.Drawing.Point(245, 250);
-            this.LoginButton.Name = "LoginButton";
-            this.LoginButton.Size = new System.Drawing.Size(104, 23);
-            this.LoginButton.TabIndex = 5;
-            this.LoginButton.Text = "Login";
-            this.LoginButton.UseVisualStyleBackColor = true;
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(245, 279);
+            this.button2.Location = new System.Drawing.Point(250, 279);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(104, 23);
             this.button2.TabIndex = 6;
             this.button2.Text = "Create Account";
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(250, 250);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(104, 23);
+            this.LoginButton.TabIndex = 5;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(251, 221);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(100, 23);
+            this.PasswordTextBox.TabIndex = 4;
+            // 
+            // UsernamTextBox
+            // 
+            this.UsernamTextBox.Location = new System.Drawing.Point(251, 183);
+            this.UsernamTextBox.Name = "UsernamTextBox";
+            this.UsernamTextBox.Size = new System.Drawing.Size(100, 23);
+            this.UsernamTextBox.TabIndex = 3;
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(182, 224);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(60, 15);
+            this.PasswordLabel.TabIndex = 2;
+            this.PasswordLabel.Text = "Password:";
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.AutoSize = true;
+            this.UsernameLabel.Location = new System.Drawing.Point(182, 186);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(63, 15);
+            this.UsernameLabel.TabIndex = 1;
+            this.UsernameLabel.Text = "Username:";
+            // 
+            // SloganText
+            // 
+            this.SloganText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.SloganText.Location = new System.Drawing.Point(109, 136);
+            this.SloganText.Name = "SloganText";
+            this.SloganText.Size = new System.Drawing.Size(374, 23);
+            this.SloganText.TabIndex = 0;
+            this.SloganText.Text = "Don\'t weather the storm alone - connect with others on Storm Social.";
             // 
             // Form1
             // 
